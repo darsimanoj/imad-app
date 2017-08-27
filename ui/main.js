5a5b91e88c1175a596e5bdc79917698aeb95ca18
darsimanoj <tharunagupta@gmail.com>
2017-08-27 18:25:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,19 @@ img.onclick=function(){
     var interval=setInterval(moveRight,50);
 };
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick =function(){
-    counter=counter+1;
-    var span =document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request =new XMLHttpRequest();
+    request.onreadystatechage=function(){
+        if(request.readyState === XMLHttprequest.DONE){
+            if(request.status ===200){
+                var counter =request.responseText;
+                
+                var span =document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://tharunagupta.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
