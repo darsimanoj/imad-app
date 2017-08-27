eb8c0a7e6ee5bc461f7a9f0f9c9cc186c3060d72
darsimanoj <tharunagupta@gmail.com>
2017-08-27 17:47:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,11 @@ function createTemplate(data){
            return htmlTemplate;
     
 }
+var counter=0;
 
+app.get('/counter', function (req, res) {
+  res.send(counter.toString());
+});
 
 
 app.get('/', function (req, res) {
