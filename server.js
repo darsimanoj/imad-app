e68c1febfacd3d99aaa1d600bfb1f3cda5dd9611
darsimanoj <tharunagupta@gmail.com>
2017-08-29 19:04:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var names = [];
-app.get('/submit-name?name',function(req,res){
+app.get('/submit-name',function(req,res){
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
